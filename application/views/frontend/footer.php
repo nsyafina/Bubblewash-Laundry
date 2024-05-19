b44105935741c33b862ebda80eb37fd7b1d2df71
nsyafina <najlasfina13@gmail.com>
2024-05-19 16:30:12 +0700

    project laundry

---
+++ application/views/frontend/footer.php
@@ -0,0 +1,51 @@
+<!-------------------------------------------------------------- Footer ----------------------------------------------------------->
+  <footer class="footer-distributed">
+    <div class="footer-left">
+      <img src="img/logo/long_logo.jpg" style="width: 330px;">
+      <p class="footer-links">
+        <a href="index.php">Home</a>
+        |
+        <a href="<?= base_url('service'); ?>">Services</a>
+        |
+        <a href="<?= base_url('histori'); ?>">Transaction History</a>
+        |
+        <a href="https://maps.app.goo.gl/9z7noQ9cd1tiwjq56">Outlet</a>
+      </p>
+      <p class="footer-company-name">Copyright &copy; 2024 - <strong>BubbleWash</strong>,  All Rights Reserved. </p>
+      <div class="footer-icons">
+        <ul>
+          <li><a href="https://www.facebook.com/ubsi.kaliabang?locale=id_ID"><i class="fa-brands fa-square-facebook"></i></a></li>
+          <li><a href="https://www.instagram.com/bsi_kaliabang/"><i class="fa-brands fa-instagram"></i></a></li>
+          <li><a href="https://www.tiktok.com/@ubsi_official"><i class="fa-brands fa-tiktok"></i></a></li>
+          <li><a href="https://wa.me/6285779082884?text=Mohon info tentang produk"><i class="fa-brands fa-whatsapp"></i></a></li>
+          <li><a href="https://t.me/+dYUvRJzGAx1lZWU1"><i class="fa-brands fa-telegram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="footer-center">
+      <div>
+        <i class="fa-solid fa-location-dot"></i>
+        <p><span>BubbleWash Jakarta</span>
+         Jl. Raya Casablanca No.88, Jakarta.</p>
+      </div>
+      <div>
+        <i class="fa-solid fa-fax"></i>
+        <p>(021) 21236158</p>
+      </div>
+      <div>
+        <i class="fa-regular fa-envelope"></i>
+        <p><a href="#">bubblewashjakarta@gmail.com</a></p>
+      </div>
+    </div>
+
+    <div class="footer-right">
+      <p class="footer-company-about">
+        Available Payment :
+        <a href="#"><img src="img/home/pay_footer.jpg" style="width: 350px;"></a>
+        Download Our App :
+        <a href="#"><img src="img/home/app_footer.jpg" style="width: 350px;"></a>
+      </p>
+    </div>
+  </footer>
+  <!------------------------------------------------------------ Footer End -------------------------------------------------------->
