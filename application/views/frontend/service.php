36829cd8b2590918afbac4bb21ad9bf43e1db306
nsyafina <najlasfina13@gmail.com>
2024-05-21 17:14:26 +0700

    project laundry

---
+++ application/views/frontend/service.php
@@ -0,0 +1,176 @@
+<!------------------------------------------------------------ Service ------------------------------------------------------------>
+<div class="container container-navtabs mt-3">
+  <nav class="nav-layanan">
+    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+      <button class="nav-link menu-layanan active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Unit Laundry</button>
+      <button class="nav-link menu-layanan" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Package Laundry</button>
+    </div>
+  </nav>
+  </div>
+  <div class="tab-content" id="nav-tabContent">
+    <!-- Unit -->
+    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+      <div class="allservice mb-4">
+        <h1 class="judul-layanan">─ Unit Laundry ─</h1>
+        <div class="container mt-4 mb-5">
+          <div class="row row-cols-1 row-cols-md-5 g-2">
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/kaos.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN001 | Kaus</h5>
+                  <p class="card-text harga-layanan">IDR. 10,000.-</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/kemeja.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN002 | Kemeja</h5>
+                  <p class="card-text harga-layanan">IDR. 12,000.-</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/jaket.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN003 | Jaket</h5>
+                  <p class="card-text harga-layanan">IDR. 15,000.-</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/celanakargo.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN004 | Celana</h5>
+                  <p class="card-text harga-layanan">IDR. 11,000.-</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/rok.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN005 | Rok</h5>
+                  <p class="card-text harga-layanan">IDR. 10,000.-</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row row-cols-1 row-cols-md-5 g-2 mt-1">
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/jumpsuit.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN006 | Jumpsuit</h5>
+                  <p class="card-text harga-layanan">IDR. 15,000.-</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/dress.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN007 | Dress</h5>
+                  <p class="card-text harga-layanan">IDR. 15,000.-</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/kebaya.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN008 | Kebaya</h5>
+                  <p class="card-text harga-layanan">IDR. 30,000.-</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/jas.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN009 | Jas/Blazer</h5>
+                  <p class="card-text harga-layanan">IDR. 25,000.-</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/rompi.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">JN010 | Rompi</h5>
+                  <p class="card-text harga-layanan">IDR. 13,000.-</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- Unit -->
+    <!-- Package -->
+    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+      <div class="allservice mb-4">
+        <h1 class="judul-layanan">─ Package Laundry ─</h1>
+        <div class="container mt-4 mb-5">
+          <div class="row row-cols-1 row-cols-md-5 g-2">
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/dry.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">Dry Clean</h5>
+                  <p class="card-text harga-layanan">IDR. 25,000.-/kg</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/wet.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">Wet Clean</h5>
+                  <p class="card-text harga-layanan">IDR. 10,000.-/kg</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/cucisetrika.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">Cuci + Setrika</h5>
+                  <p class="card-text harga-layanan">IDR. 25,000.-/kg</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/setrika.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">Only Setrika</h5>
+                  <p class="card-text harga-layanan">IDR. 10,000.-/kg</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card layanan">
+                <img src="img/service/self.jpg" class="card-img-top img-layanan" alt="...">
+                <div class="card-body body-layanan">
+                  <h5 class="card-title title-layanan">Self Service</h5>
+                  <p class="card-text harga-layanan">IDR. 5,000.-/kg</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Package -->
+  </div>
+
+
+  
+
+   
